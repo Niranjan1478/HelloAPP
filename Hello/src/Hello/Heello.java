911adebbabf8dcf6a263d40aa32cd56c79cb3bd7
Niranjan1478 <niranjan.chennaka444@gmail.com>
2022-11-27 21:03:52 +0530

    Hello

--- Hello/src/Hello/Heello.java
+++ Hello/src/Hello/Heello.java
@@ -3,7 +3,9 @@ package Hello;
 public class Heello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Woeld");
+		//System.out.println("Hello Woeld");
+		System.out.println("Hello Java");
+		
 		// TODO Auto-generated method stub
 
 	}
